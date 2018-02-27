0b209685c466856691cc2269b94427eecf1433d5
Trey Pendragon <tpendragon@princeton.edu>
2018-02-27 10:48:53 -0800

    Initial commit.

---
+++ lib/valkyrie/derivatives.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require "valkyrie/derivatives/version"
+
+module Valkyrie
+  module Derivatives
+    # Your code goes here...
+  end
+end
---
+++ spec/valkyrie/derivatives_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+RSpec.describe Valkyrie::Derivatives do
+  it "has a version number" do
+    expect(Valkyrie::Derivatives::VERSION).not_to be nil
+  end
+end
